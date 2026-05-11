48e0791d3c728039d3be5db5230d7b468b6b2029
Roger Deng <13251150+rogerdigital@users.noreply.github.com>
2026-05-11 16:23:59 +0800

    feat: add strategy lifecycle contracts

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -1,5 +1,6 @@
 export * from './data-science.ts';
 export * from './errors.ts';
 export * from './experiments.ts';
+export * from './lifecycle.ts';
 export * from './research.ts';
 export * from './trading.ts';
